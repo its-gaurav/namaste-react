2112d44fe7d4b7fcf398976d61860941bce22bf8
Gaurav Raj <gauravraj911@gmail.com>
2026-03-17 23:50:42 +0530

    added initial changes

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>This is h1 tag</h1>
+ *          <h2>This is h2 tag</h2>
+ *      </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "This is h1 tag"),
+        React.createElement("h2", {}, "This is h2 tag")
+    ]
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
